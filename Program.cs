07bf8bc8248c6d4bd5b0e68b2527999f491db8dc
Asvin Sayanthan <asvin1010@gmail.com>
2025-02-18 14:52:23 -0500

    feb14backup3

---
+++ Program.cs
@@ -0,0 +1,211 @@
+using System;
+using System.Threading;
+using HoustonRadarLLC.StatsAnalyzer;
+using HoustonRadarLLC.Models.Comm;
+using HoustonRadarLLC.DAL.Comm;
+using HoustonRadarLLC.Models;
+using System.Collections.Generic;
+using HoustonRadarLLC.StatsAnalyzer.Data;
+using System.IO;
+using Newtonsoft.Json;
+ 
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using HoustonRadarLLC.DAL.Comm;
+using HoustonRadarLLC.Models.Comm;
+using System.IO;
+ 
+namespace HoustonRadarCSharpAppEx
+{
+    class Program
+    {
+        private static radar_generic genericRdr;
+        static ManualResetEvent pauseEvent = new ManualResetEvent(false);
+        //private static radarCommClassThd rdr = new radarCommClassThd(null);
+        private static int totalbytes = 0;
+        private static int totalpackets = 0;
+ 
+        private static DateTime end = DateTime.Now.AddSeconds(-1);
+        private static DateTime start = end.AddMinutes(-15);
+ 
+        private static int[] radarIPs = { 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 };
+        private static radarCommClassThd curRadar;
+ 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connection starting...");
+ 
+            for (int i = 0; i < radarIPs.Length; i++)
+            {
+                curRadar = new radarCommClassThd(null);
+                ConnectToRadar(curRadar, radarIPs[i]);
+            }
+ 
+            pauseEvent.WaitOne();
+            Console.WriteLine("Connection completed.");
+ 
+            Thread.Sleep(5000);
+        }
+ 
+        static async Task ConnectToAPI()
+        {
+            string url = "https://api.spectrumtraffic.com/radar.php?act=get_schedules&ip_address=161.184.106.40";
+ 
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+ 
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Request error: {e.Message}");
+                }
+            }
+            Thread.Sleep(9000);
+        }
+ 
+        private static void ConnectToRadar(radarCommClassThd rdr, int ip)
+        {
+            rdr.IPaddr = "161.184.106." + ip.ToString();
+            rdr.portnum = 5125;
+            rdr.DoSerialConnect = false;
+            rdr.ReadTimeout = 2000;
+            rdr.WriteTimeout = 2000;
+ 
+            rdr.RadarEventRadarFound += rdr_RadarEventRadarFound;
+ 
+            // Execute readData() when connection setup completes
+            rdr.RadarEventGetInfoDone += (sender, e) =>
+            {
+                Console.WriteLine("Radar connection established. Now reading data from radar " + rdr.IPaddr);
+                readData(rdr, ip);
+            };
+ 
+            rdr.RadarEventRadarNotFound += rdr_RadarEventRadarNotFound;
+            rdr.RadarEventEx += rdr_RadarEventEx;
+            rdr.RadarEventCommErr += rdr_RadarEventCommErr;
+            rdr.RadarEventOOBdata += rdr_RadarEventOOBdata;
+ 
+            rdr.Connect();
+        }
+ 
+ 
+        private static void readData(radarCommClassThd rdr, int ip)
+        {
+ 
+            HoustonRadarLLC.speedLanePreformedQueries schema = new HoustonRadarLLC.speedLanePreformedQueries(rdr);
+            schema.ReadVehiclesDateRange(start, end);
+ 
+            schema.progressevent += new HoustonRadarLLC.speedLanePreformedQueries.progressdelegate(schema_progressevent);
+            schema.progressPctComplete += new HoustonRadarLLC.speedLaneSchema.progressPctCompleteDelegate(schema_progressPctComplete);
+ 
+            string speedUnit = "km/h";
+            string lengthUnit = "m";
+ 
+            // Look at individual vehicles and print date/timestamp, lane, speed, length
+            HoustonRadarLLC.speedLanePreformedQueries.speedLaneVehicleRec[] vehicles = schema.getSpeedLaneVehicles();
+            Console.WriteLine("Number of vehicles: " + vehicles.Length);
+            parseAndPrintVehicles(speedUnit, lengthUnit, vehicles, ip);
+        }
+ 
+        private static void schema_progressevent(int packetno, int bytes)
+        {
+            totalbytes += bytes;
+            totalpackets++;
+            Console.WriteLine("Pkt #" + totalpackets + " (Total:" + ((float)totalbytes / 1024.0).ToString("0.0") + "kB)");
+        }
+ 
+        private static void schema_progressPctComplete(int pct)
+        {
+            Console.WriteLine(pct.ToString());
+        }
+ 
+        private static void parseAndPrintVehicles(string speedUnit, string lengthUnit, HoustonRadarLLC.speedLanePreformedQueries.speedLaneVehicleRec[] vehicles, int ip)
+        {
+            Console.WriteLine("apwiehrwpiqhrqpwe");
+            using (StreamWriter sw = new StreamWriter(DateTime.Now.ToString("yyyyMMdd-hhmmsstt") + ".json"))
+            {
+                sw.WriteLine("Ip address: 161.184.106." + ip);
+                sw.WriteLine("[");
+                foreach (HoustonRadarLLC.speedLanePreformedQueries.speedLaneVehicleRec rec in vehicles)
+                {
+                    sw.WriteLine(
+                        "\t{" +
+                        "\n\t\tTimeEnding: " + rec._dt.ToString() +
+                        "\n\t\tLane: " + rec._lane +
+                        "\n\t\tSpeed: " + rec._speed.ToString() + " " + speedUnit +
+                        "\n\t\tLength: " + (Math.Round(rec._length) / 100.0).ToString() + " " + lengthUnit +
+                        "\n\t\tDirection: " + rec._direction.ToString() +
+                        "\n\t},"
+                    );
+                }
+                sw.WriteLine("]");
+            }
+        }
+ 
+ 
+        private static void rdr_RadarEventRadarFound(object sender, RadarCommEventArgs e)
+        {
+            Console.WriteLine("Radar was successfully pinged!");
+        }
+ 
+        private static void rdr_RadarEventGetInfoDone(object sender, RadarCommEventArgs e)
+        {
+ 
+            genericRdr = (new radar_speedlane(curRadar)) as radar_speedlane;
+            genericRdr.progressupdate += new progressupdatehandler(varRead_progressupdate);
+ 
+            string err;
+            if (!genericRdr.filldata(out err))
+                Console.WriteLine("Error: " + err);
+ 
+            genericRdr.progressupdate -= new progressupdatehandler(varRead_progressupdate);
+            Console.WriteLine("Radar system data has been successfully recieved.");
+ 
+            radar_generic.RadarConfig radarCfg = genericRdr.GetAllConfig();
+ 
+        }
+ 
+        private static void varRead_progressupdate(int pct, string var, string val, progressenum state)
+        {
+ 
+            //Console.WriteLine(pct);
+ 
+            if (state == progressenum.stop)
+            {
+                Console.WriteLine("Progress update complete");
+ 
+                if (!pauseEvent.WaitOne(0))
+                    pauseEvent.Set();
+            }
+        }
+ 
+        private static void rdr_RadarEventRadarNotFound(object sender, RadarCommEventArgs e)
+        {
+            Console.WriteLine("Radar not found.");
+            pauseEvent.Set();
+        }
+ 
+        private static void rdr_RadarEventCommErr(object sender, RadarCommEventArgs e)
+        {
+            Console.WriteLine("Communication error: " + e.CommErrStr);
+        }
+ 
+        private static void rdr_RadarEventEx(object sender, RadarCommEventArgs e)
+        {
+            Console.WriteLine("Exception: " + e.ex.Message);
+        }
+ 
+        private static void rdr_RadarEventOOBdata(object sender, RadarCommEventArgs e)
+        {
+            Console.WriteLine("Out of Band Data: " + e.oobdata);
+        }
+    }
+}
