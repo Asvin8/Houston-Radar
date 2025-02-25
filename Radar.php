742c3349a1dc76eaa42c383f2b0019a10f0f2960
Asvin Sayanthan <asvin1010@gmail.com>
2025-02-24 19:48:52 -0500

    decompress and print gzip file
    copied mostly from chatgpt, don't trust fully

--- Radar.php
+++ Radar.php
@@ -17,12 +17,12 @@ header("access-control-allow-methods: GET,HEAD,PUT,PATCH,POST,DELETE");
 header("access-control-allow-origin: http://localhost:3001");
 
 // display errors
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 
-$_POST = json_decode(file_get_contents("php://input"), true);
+//$_POST = json_decode(file_get_contents("php://input"), true);
 
 //Check Which Action To Perform:
 if (isset($_POST['act'])) { $act = trim($_POST['act']); }
@@ -62,7 +62,61 @@ switch ($act) {
     echo json_encode($resultant);
     exit(0);
 
-    break;
+    case "upload_gzip":
+
+      // check if file was uploaded
+      if(!isset($_FILES['files'])) {
+        $resultant['err'] = 1;
+        $resultant['message'] = "no file was sent to API";
+        echo json_encode($resultant);
+        exit(0);
+      }
+
+      // only accept gzip file
+      $fileType = $_FILES['file']['type'];
+      if($fileType !== "application/gzip") {
+        $resultant['err'] = 1;
+        $resultant['message'] = "Invalid file type";
+        echo json_encode($resultant);
+        exit(0);
+      }
+
+      // save uploaded file
+      $uploadDir = __DIR__ . "/uploads/";
+      if(!is_dir($uploadDir)) { mkdir($uploadDir, 0777, true); }
+
+      // check if file upload failed
+      $uploadedFilePath = $uploadDir . basename($_FILES['file']['name']);
+      if(!move_uploaded_file($_FILES['file']['tmp_name'], $uploadedFilePath)) {
+        $resultant['err'] = 1;
+        $resultant['message'] = "File failed to upload";
+        echo json_encode($resultant);
+        exit(0);
+      }
+
+      // decompress file
+      $jsonFilePath = str_replace(".gz", "", $uploadedFilePath);
+      $gzFile = gzopen($uploadedFilepath, 'rb');
+      if(!$gzFile) {
+        $resultant['err'] = 1;
+        $resultant['message'] = "Failed to open gz file";
+        echo json_encode($resultant);
+        exit(0);
+      }
+
+      // parse gz zip file into JSON
+      $jsonContent = "";
+      while(!gzeof(gzFile)) { $jsonContent .= gzread($gzFile, 4096);  }
+      gzclose($gzFile);
+
+      // print JSON info
+      $resultant['err'] = 0;
+      $resultant['message'] = "File uploaded and decompressed successfully";
+      $resultant['file_path'] = $uploadedFilePath;
+      $resultant['json_contents'] = json_decode($jsonContent, true);
+      echo json_encode($resultant, JSON_PRETTY_PRINT);
+      exit(0);
+      
 
   default:
 
